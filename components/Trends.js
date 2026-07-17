000b78a0574f45d581509ea0a179df44e8dabb82
Murielle <mnouatin@gmail.com>
2026-07-17 11:57:44 +0200

    gestion de la navigation vers home au clic sur le logo

--- components/Trends.js
+++ components/Trends.js
@@ -1,28 +1,32 @@
 // components/Trends.js
 import { useEffect, useState } from "react";
-import styles from '../styles/Trends.module.css';
+import styles from "../styles/Trends.module.css";
 
-function Trends() {
+function Trends({ tweets }) {
   const [trends, setTrends] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/tweets/trends")
       .then((res) => res.json())
       .then((data) => setTrends(data.trends));
-  }, []);
+  }, [tweets]);
 
   return (
-    <aside className="trends">
-      <h3>Trends</h3>
-      <ul>
+    <div className={styles.trendsContainer}>
+      <h3 className={styles.title}>Trends</h3>
+      <div className={styles.trendsBox}>
         {trends.map((t, index) => (
-          <li key={index}>
-            #{t.hashtag} — {t.count}
-          </li>
+          <div key={index} className={styles.trendItem}>
+            <p className={styles.hashtag}>#{t._id}</p>
+            <p className={styles.count}>{t.count} Tweet{t.count > 1 ? "s" : ""}</p>
+          </div>
         ))}
-      </ul>
-    </aside>
+      </div>
+    </div>
   );
 }
 
 export default Trends;
+
+
+ 
