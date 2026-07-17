5b0c537201702a644c109e2f53a74fdb8cb749d7
Cynthia <cynthia.philippe@gmail.com>
2026-07-17 11:31:55 +0200

    correction routes dans lastTweet et className entre accolades

--- components/LastTweets.js
+++ components/LastTweets.js
@@ -7,7 +7,7 @@ function LastTweets({ tweets }) {
   const user = useSelector((state) => state.user.value);
 
   const handleLike = (tweetId) => {
-    fetch(`http://localhost:3000/tweets/like/${tweetId}`, {
+    fetch(`http://localhost:3000/tweets/${tweetId}/like`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ token: user.token }),
@@ -15,7 +15,7 @@ function LastTweets({ tweets }) {
   };
 
   const handleDelete = (tweetId) => {
-    fetch(`http://localhost:3000/tweets/delete/${tweetId}`, {
+    fetch(`http://localhost:3000//tweets/${tweetId}`, {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ token: user.token }),
@@ -23,29 +23,29 @@ function LastTweets({ tweets }) {
   };
 
   return (
-    <div className="styles.last-tweets">
+    <div className="{styles.last-tweets}">
       <h3>Last tweets</h3>
 
-      <div className="styles.tweets-list">
+      <div className="{styles.tweets-list}">
         {tweets.map((tweet) => {
           const isOwner = tweet.user.username === user.username;
 
           return (
-            <div key={tweet._id} className="styles.tweet-item">
+            <div key={tweet._id} className="{styles.tweet-item}">
               <p>
                 <strong>{tweet.user.firstname}</strong> @{tweet.user.username}
               </p>
 
               <p>{tweet.content}</p>
 
-              <div className="styles.tweet-actions">
+              <div className="{styles.tweet-actions}">
                 <button onClick={() => handleLike(tweet._id)}>
                   ♡ {tweet.likes.length}
                 </button>
 
                 {isOwner && (
                   <button
-                    className="styles.delete-btn"
+                    className="{styles.delete-btn}"
                     onClick={() => handleDelete(tweet._id)}
                   >
                     🗑️
